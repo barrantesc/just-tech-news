9aa65a5d0e088a0d2594c26ac1099abd0f1f1222
playground <connie.s.barrantes@gmail.com>
2022-01-25 09:02:20 -0500

    test 2

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -6,22 +6,25 @@ const { Post, User } = require('../../models');
 
 // router will retrieve all post in database
 // get all users
-// router.get('/', (req, res) => {
-//     Post.findAll({
-//       attributes: ['id', 'post_url', 'title', 'created_at'],
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['username']
-//         }
-//       ]
-//     })
-//       .then(dbPostData => res.json(dbPostData))
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      // Query configuration
+      attributes: ['id', 'post_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
 
 //test route
 module.exports = router;

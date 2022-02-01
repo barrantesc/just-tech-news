a683b7dbc31ebf5eab2755392511befcb0105a79
playground <connie.s.barrantes@gmail.com>
2022-01-31 23:38:36 -0500

    changed error on index.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
 
 const app = express();
@@ -8,24 +7,17 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-// set up Handlebars.js start
-
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-// ending of handlebars.js set up 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers/'));
 
-
-// turn on connection to db and server
-// changing force to true will connect database to sync with model definitions and associations
-// forcing to true will make tables re-create if there are any association changes. 
-// true only t test if user tables drop, switch back to false
-sequelize.sync({ force: true}).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
